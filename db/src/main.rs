9aeb392b695c40ccde57aa91c0ddf761b68f0fb6
John Fernandez <jafernandez.tampabay@gmail.com>
2023-08-12 17:13:22 -0400

    remove this for now
    I'll place a proper implementation when we try to implement command line arguments

--- db/src/main.rs
+++ db/src/main.rs
@@ -6,18 +6,6 @@ mod db_verify;
 use clap::Parser;
 mod tests;
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
-}
 
 
 fn main() {
