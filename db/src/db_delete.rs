0ba8e991b705b6eb6d637634cfd5adafa0740228
John Fernandez <jafernandez.tampabay@gmail.com>
2023-08-16 00:03:58 -0400

    Create a delete administrator function.

--- db/src/db_delete.rs
+++ db/src/db_delete.rs
@@ -1,6 +1,6 @@
 use sqlite;
 use crate::db_verify::V;
-
+use crate::db_retrieve::R;
 
 pub struct D {}
 
@@ -24,5 +24,45 @@ impl D {
         }
 
     }
+
+    pub fn delete_administrator(connection : &sqlite::Connection, admin_id: i64) -> bool {
+
+        // if so, this user didn't really exist in the first place, go figure.
+        if !V::check_id(connection, admin_id, V::ADMINISTRATORS) {
+            return false;
+        }
+        let query = "DELETE FROM administrators WHERE admin_id = ".to_owned()
+        + &admin_id.to_string();
+
+        match connection.execute(query){
+            Ok(_) => return true,
+            Err(_) => return false
+        }
+
+    }
+
+    pub fn delete_school(connection : &sqlite::Connection, school_id: i64) -> bool {
+
+        // if so, this user didn't really exist in the first place, go figure.
+        if !V::check_id(connection, school_id, V::SCHOOLS) {
+            return false;
+        }
+
+        // only delete a school if there are no associated classes
+        match R::retrieve_classes_from_school(connection, school_id) {
+            Ok(values) => if values.into_iter().next().is_none() {return false},
+            Err(_) =>  return false
+        }
+
+        let query = "DELETE FROM schools WHERE school_id = ".to_owned()
+        + &school_id.to_string();
+
+        match connection.execute(query){
+            Ok(_) => return true,
+            Err(_) => return false
+        }
+
+    }
+
 
 }
